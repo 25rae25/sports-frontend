7467980926c9e240f11a35dd3293538635718239
25rae25 <whdudfo0124@gmail.com>
2023-04-17 18:35:00 +0900

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { useThemeContext } from './context/themeContext';
 import { lightTheme,darkTheme } from './styles/theme';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
+import Main from './components/Main';
 
-function App() {
+const App = () => {
   const { theme } = useThemeContext();
   return (
    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <GlobalStyle></GlobalStyle>
+      <GlobalStyle />
+        <Main />
     </ThemeProvider>
   );
 }
---
+++ src/components/Siderbar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	position: relative;
+`;
+
+const NavBar = styled.div`
+	width:300px;
+	height: 100%;
+	background-color: ${({theme}) => theme.secondary};
+`
+
+const ContentContainer = styled.div`
+	width: 100%;
+	height: 100%;
+`
+
+interface Props {
+	children: JSX.Element;
+}
+
+const Sidebar = ({children}: Props) => {
+	return (
+		<Container>
+			<NavBar></NavBar>
+			<ContentContainer>{children}</ContentContainer>
+		</Container>
+	)
+}
+
+export default Sidebar;
