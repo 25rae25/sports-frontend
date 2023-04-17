0723f12a964b7c169d7c8e1d78fd49eeb43b48c2
25rae25 <whdudfo0124@gmail.com>
2023-04-17 18:06:13 +0900

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { TodoListItem } from '../src/components/TodoListItem';
-import { Todo } from './components/Types';
+import './App.css';
 
-const todos: Array<Todo> = [{text: "밥 먹기", complete: true}, {text: "옷 입기", complete: false}];
 
-const App: React.FC = () => {
+function App() {
   return (
-    <ul>
-    <React.Fragment>
-      <TodoListItem todo={todos[0]}/>
-      <TodoListItem todo={todos[1]}/>
-    </React.Fragment>
-    </ul>
-
-   );
+   <div>안녕</div>
+  );
 }
 
 export default App;
