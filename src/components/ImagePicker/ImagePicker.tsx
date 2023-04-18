3271d4c77e44e51af0e08d8e1091c09d3da09e91
25rae25 <whdudfo0124@gmail.com>
2023-04-18 23:57:19 +0900

    antd-design

---
+++ src/components/ImagePicker/ImagePicker.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import {AliwangwangOutlined } from '@ant-design/icons';
+
+const ImagePicker = () => {
+	return(
+		<div><AliwangwangOutlined />;</div>
+	);
+}
+
+export default ImagePicker;
---
+++ src/data/Images.ts
@@ -0,0 +1,3 @@
+export const images: string[] = [
+	// 이미지
+  ];
