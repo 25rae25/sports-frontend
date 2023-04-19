94e4e0484a8a4e84249926a1a57dfd6b77b9bda4
25rae25 <whdudfo0124@gmail.com>
2023-04-19 17:04:13 +0900

    aboutDetail연결

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Sidebar from '../Siderbar';
+import AboutDetail from './AboutDetail';
 
 const About = () => {
 	return (
 		<Sidebar>
-			<>About</>
+			<AboutDetail />
 		</Sidebar>
 	)
 }
---
+++ src/components/About/AboutDetail.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AboutDetail = () => {
+	return(
+		<div>소개글</div>
+	)
+}
+
+export default AboutDetail;
