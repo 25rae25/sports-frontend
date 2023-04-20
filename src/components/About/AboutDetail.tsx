37a73043e413a8e7207a32939470f550bb5b14e2
25rae25 <whdudfo0124@gmail.com>
2023-04-20 16:06:51 +0900

    About 수정

--- src/components/About/AboutDetail.tsx
+++ src/components/About/AboutDetail.tsx
@@ -1,8 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+import AboutInfo from "./AboutInfo";
+
+const Container = styled.div`
+	width: 1200px;
+	height: 600px;
+	margin: 100px auto 0px auto;
+	border:1px solid red;
+	display: flex;
+`
+
+const AboutImg = styled.div`
+	width: 600px;
+	border: 1px solid blue;
+`
+
+const Aboutinfo = styled.div`
+	width: 600px;
+	border: 1px solid green;
+	display: flex;
+	flex-direction: column;
+`
+
+const Abouttitle = styled.div`
+	width: 100%;
+	height: 300px;
+	border: 1px solid green;
+`
 
 const AboutDetail = () => {
 	return(
-		<div>소개글</div>
+		<Container>
+			<AboutImg />
+			<Aboutinfo>
+				<Abouttitle>안녕하세요 조영래입니다</Abouttitle>
+				<AboutInfo />
+			</Aboutinfo>
+		</Container>
 	)
 }
 
---
+++ src/components/About/AboutInfo.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const AboutInfo = () => {
+	return(
+		<div>
+			<div>이름 : 조영래</div>
+			<div>나이 : 27살</div>
+			<div>이메일 : whdudfo0124@gmail.com</div>
+			<div>git : https://github.com/25rae25</div>
+			<div>블로그 : https://velog.io/@niceyoungrae</div>
+		</div>
+	)
+}
+
+export default AboutInfo;
