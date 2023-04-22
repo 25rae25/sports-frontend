a3ff9ca24d2b04356145965585d1b235dbb929f5
25rae25 <whdudfo0124@gmail.com>
2023-04-22 13:46:58 +0900

    login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
+import Sidebar from "../Siderbar";
 
 type Inputs = {
     email: string
@@ -21,12 +22,14 @@ export default function FormSignUp() {
     // const onSubmit = (data) => console.log(data)
 
     return (
-        <form /*onSubmit={handleSubmit(onSubmit)}*/>
-        <input {...register('email')} />
-        {errors.email?.message}
-        <input {...register('password')} />
-        {errors.password?.message}
-        <input type='submit' />
-    </form>
+        <Sidebar>
+            <form /*onSubmit={handleSubmit(onSubmit)}*/>
+                <input {...register('email')} />
+                {errors.email?.message}
+                <input {...register('password')} />
+                {errors.password?.message}
+                <input type='submit' />
+            </form>
+    </Sidebar>
     )
 }
