fe206c569c3c14425baef503a62bc9cce158469b
25rae25 <whdudfo0124@gmail.com>
2023-05-02 17:19:00 +0900

    회원가입

--- src/components/SignUp/SignupForm.tsx
+++ src/components/SignUp/SignupForm.tsx
@@ -1,5 +1,12 @@
+import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import styled from 'styled-components';
+
+const ErrorMessage = styled.div`
+	color: red;
+	font-size: 0.8rem;
+`
 
 interface SignupFormValues {
   name: string;
@@ -7,10 +14,10 @@ interface SignupFormValues {
   password: string;
 }
 
-const SignupFormSchema = Yup.object().shape({
+const schema = Yup.object().shape({
   name: Yup.string().required('이름을 입력해주세요'),
   email: Yup.string().email('이메일을 입력해주세요').required('이메일형식이 올바르지 않습니다'),
-  password: Yup.string().required('비밀번호를 입력해주세요'),
+  password: Yup.string().min(8, '비밀번호 8자리 이상 입력해주세요').required('비밀번호를 입력해주세요'),
 });
 
 const SignupForm = () => {
@@ -20,7 +27,7 @@ const SignupForm = () => {
       email: '',
       password: '',
     },
-    validationSchema: SignupFormSchema,
+    validationSchema: schema,
     onSubmit: (values) => {
       console.log(values);
     },
@@ -29,47 +36,50 @@ const SignupForm = () => {
   return (
     <form onSubmit={formik.handleSubmit}>
       <div>
-        <label htmlFor="name">Name</label>
+        <label htmlFor="name">이름</label>
         <input
           type="text"
           id="name"
           name="name"
           value={formik.values.name}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
         {formik.touched.name && formik.errors.name && (
-          <div>{formik.errors.name}</div>
+          <ErrorMessage>{formik.errors.name}</ErrorMessage>
         )}
       </div>
       <div>
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email">이메일</label>
         <input
           type="email"
           id="email"
           name="email"
           value={formik.values.email}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
         {formik.touched.email && formik.errors.email && (
-          <div>{formik.errors.email}</div>
+          <ErrorMessage>{formik.errors.email}</ErrorMessage>
         )}
       </div>
       <div>
-        <label htmlFor="password">Password</label>
+        <label htmlFor="password">비밀번호</label>
         <input
           type="password"
           id="password"
           name="password"
           value={formik.values.password}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
         {formik.touched.password && formik.errors.password && (
-          <div>{formik.errors.password}</div>
+          <ErrorMessage>{formik.errors.password}</ErrorMessage>
         )}
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit">회원가입</button>
     </form>
   );
 };
 
-export default SignupForm;
+export default SignupForm
