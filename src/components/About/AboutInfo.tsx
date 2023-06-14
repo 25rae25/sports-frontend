d6b1298e5d41748c6196fa7ab2fcf93139dc220c
25rae25 <whdudfo0124@gmail.com>
2023-06-14 13:01:29 +0900

    css추가

--- src/components/About/AboutInfo.tsx
+++ src/components/About/AboutInfo.tsx
@@ -17,6 +17,7 @@ const AboutInfo = () => {
 			<About>연고지 : 잉글랜드 런던 해링게이 구 (Haringey) 토트넘 (Tottenham)</About>
 			<About>소속리그 : 프리미어 리그 (Premier League)</About>
 			<About>홈구장 : 토트넘 홋스퍼 스타디움</About>
+			<About>별칭 : 스퍼스 (Spurs)릴리화이츠 (The Lilywhites)</About>
 		</Aboutwrap>
 	)
 }
